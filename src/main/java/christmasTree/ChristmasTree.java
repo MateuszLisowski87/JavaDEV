013512add5fbc2f2a5defff2149470d9aa923afd
LAPTOP-9HG12T71\mateu <mateusz.lisow@gmail.com>
2018-03-11 10:27:06 +0100

    Set the symbol for vertical types of tree from the keyboard

--- src/main/java/christmasTree/ChristmasTree.java
+++ src/main/java/christmasTree/ChristmasTree.java
@@ -2,11 +2,14 @@ package christmasTree;
 
 import org.apache.commons.lang3.StringUtils;
 
+import java.util.Scanner;
+
 public class ChristmasTree {
     private int level;
     private String direction;
     private String stringToDrawTheTree;
     private String temporaryStringToConstruction;
+    private String symbolForTree;
 
     public ChristmasTree(int level, String direction) {
         this.level = level;
@@ -15,20 +18,27 @@ public class ChristmasTree {
             System.out.println("Wrong direction given");
             System.exit(1);
         }
+        this.symbolForTree = setSymbolForVerticalTree();
+    }
+
+    public String setSymbolForVerticalTree() {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please get the symbol you want to use for create a tree: ");
+        return input.next();
     }
 
     public void drawTree() {
         switch (direction) {
             case "up":
                 for (int i = 0; i < level; i++) {
-                    stringToDrawTheTree = StringUtils.repeat("*", ((i * 2) + 1));
+                    stringToDrawTheTree = StringUtils.repeat(symbolForTree, ((i * 2) + 1));
                     temporaryStringToConstruction = StringUtils.repeat(" ", level - i - 1);
                     System.out.println(temporaryStringToConstruction + stringToDrawTheTree);
                 }
                 break;
             case "down":
                 for (int i = (level - 1); i > -1; i--) {
-                    stringToDrawTheTree = StringUtils.repeat("*", ((i * 2) + 1));
+                    stringToDrawTheTree = StringUtils.repeat(symbolForTree, ((i * 2) + 1));
                     temporaryStringToConstruction = StringUtils.repeat(" ", level - i - 1);
                     System.out.println(temporaryStringToConstruction + stringToDrawTheTree);
                 }
--- src/main/java/christmasTree/ChristmasTreeApp.java
+++ src/main/java/christmasTree/ChristmasTreeApp.java
@@ -2,7 +2,7 @@ package christmasTree;
 
 public class ChristmasTreeApp {
     public static void main(String[] args) {
-        ChristmasTree niceChristmasTree = new ChristmasTree(5, "right");
+        ChristmasTree niceChristmasTree = new ChristmasTree(5, "up");
         niceChristmasTree.drawTree();
     }
 }
